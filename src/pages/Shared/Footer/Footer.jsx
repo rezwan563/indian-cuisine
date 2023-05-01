660372d7c713976ff494bfb2d7748ae974a3ab60
Rezwan <t.rewan17@gmail.com>
2023-05-02 02:11:30 +0600

    Footer added

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-    return (
+  return (
+    <div>
+      <footer className="footer p-10 bg-base-200 text-base-content">
         <div>
-            <h1>This is footer</h1>
+          <span className="footer-title">Newsletter</span>
+          <a className="link link-hover">Branding</a>
+          <a className="link link-hover">Design</a>
+          <a className="link link-hover">Marketing</a>
+          <a className="link link-hover">Advertisement</a>
         </div>
-    );
+        <div>
+          <span className="footer-title">Private events</span>
+          <a className="link link-hover">About us</a>
+          <a className="link link-hover">Contact</a>
+          <a className="link link-hover">Jobs</a>
+          <a className="link link-hover">Press kit</a>
+        </div>
+        <div>
+          <span className="footer-title">Hours</span>
+          <a className="link link-hover">Terms of use</a>
+          <a className="link link-hover">Privacy policy</a>
+          <a className="link link-hover">Cookie policy</a>
+        </div>
+      </footer>
+      
+    </div>
+  );
 };
 
 export default Footer;
--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import "./Header.css"
 
 const Header = () => {
     return (
-        <div>
-            <h1>This is header</h1>
-        </div>
+        <nav className='md:flex md:justify-between md:items-center mb-8'>
+            <div className=''>
+                <div>
+                    <Link className='mr-4' to='/'>Home</Link>
+                    <Link className='mr-4' to='/about_us'>About</Link>
+
+                    <Link to='/blog'>Blog</Link>
+                </div>
+
+            </div>
+            <div>
+                <Link to='/' className='text-amber-500 text-3xl font-bold'>Indian Cuisine</Link>
+            </div>
+            <div className='flex'>
+                <div className='avatar_img'>
+                    <img className='h-10' src="/src/assets/avatar_img.jpg" alt="" />
+                    <span>Abid Rahman</span>
+                </div>
+                <button className='bg-amber-500 p-2 border'>Login</button>
+                <button className='bg-amber-500 p-2 border'>Logout</button>
+            </div>
+        </nav>
     );
 };
 
